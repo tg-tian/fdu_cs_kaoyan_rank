015b9851f55590d64408c87c28c539c900c64f26
tgapk <914837973@qq.com>
2026-02-23 12:18:03 +0800

    update:修改python日志使用上海时区

--- score_service/logger.py
+++ score_service/logger.py
@@ -1,12 +1,27 @@
 import logging
 import sys
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def setup_logging():
+    shanghai_tz = ZoneInfo("Asia/Shanghai")
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    def shanghai_converter(timestamp):
+        return datetime.fromtimestamp(timestamp, tz=shanghai_tz).timetuple()
+    
+    formatter.converter = shanghai_converter
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.StreamHandler(sys.stdout)
-        ]
+        handlers=[handler]
     )
+    
     return logging.getLogger("score_service")
